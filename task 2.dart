14d8cf5d00e311c72b4d6046b2e2e41b6452ce8c
georgekamaal814@gmail <georgekamaal814@gmail>
2026-03-02 09:23:03 +0200

    first commit

---
+++ task 2.dart
@@ -0,0 +1,25 @@
+  //  import 'dart:vmservice_io';
+ import 'dart:io';
+
+// void task1 ( ) {
+// print("enter your name and password  ");
+//     String name = stdin.readLineSync()??"name";
+//   String password = stdin.readLineSync()??"pass";
+//   bool isActive = stdin.readLineSync() =="true";
+//     name == "student"&& password=="iti123" && isActive ==true?print ('Login Successful'):print ("Access Denied");
+// }
+ void task2 ( ) {
+    
+     print ("gender is  name is  ");}
+    
+
+void main() {
+
+
+       print ("gender is  name is  ");
+
+  //  task1 ();
+
+
+
+}
